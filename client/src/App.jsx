106445e6ed4f2690539baf67394bfdc011dc89cd
bluesoltech-kunal <dev.kunal.bluesoltech@gmail.com>
2024-03-27 11:46:41 +0530

    footr

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './layout/Layout'
+// import Speakers from './Pages/Speakers'
 
 function App() {
+
+  const speaker = [
+    {
+      name: "hardik",
+      position:"jhggjg",
+      img:""
+    }
+  ]
 
   return (
     <>
       <Layout />
+      {/* <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Speakers
+       speaker = {
+        speaker
+       }
+        
+      />
+    </div> */}
     </>
   )
 }
--- client/src/Pages/Speakers.jsx
+++ client/src/Pages/Speakers.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Speakers = () => {
   return (
-    <div>Speakers</div>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      {/* <img className="w-full h-auto" src="" alt={name} />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{position}</p>
+      </div> */}
+    </div>
   )
 }
 
