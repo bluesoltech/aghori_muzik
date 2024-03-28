73b9426684d4ee28c34a0b1ecfcb17d99e05b521
hardik sinh padhiyar <114370359+hardiks7@users.noreply.github.com>
2024-03-28 12:10:52 +0530

    schedul,vid

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import Map from "../Components/Map";
 import Sponsors from "../Components/Sponsors";
 import Timmer from "../Components/Timmer";
 import Speakers from "./Speakers";
+import banner from "../assets/ARUFirst-01.jpg"
 const faqs = [
   {
     question: "5:00 pm Reporting Time",
@@ -15,37 +16,37 @@ const faqs = [
     ],
   },
   {
-    question: "5:10 event start",
+    question: "5:30 pm event Start",
     answer: [
       "SEO is one of the most important aspects of online marketing. It can help you rank higher in search engine results pages, which can lead to more website visitors and more customers. If you’re looking for an SEO package, you may be wondering how much they cost.",
     ],
   },
   {
-    question: "5:20 Acp Department Activities",
+    question: "Deep Pragatya Ceremony",
     answer: [
       "There are a lot of SEO packages on the market. But are they worth the investment?",
     ],
   },
   {
-    question: "6:00 Glimpse of Acp department",
+    question: "Award Ceremony",
     answer: [
       "SEO is not a one-time job. It’s an ongoing process that needs to be tweaked and adjusted as search engine algorithms change. That’s why most SEO professionals recommend monthly SEO services.",
     ],
   },
   {
-    question: "6:15 guest welcome with Diya lightning ceremony",
+    question: "Glimps of Ahmedabad City Police",
     answer: [
       "Yes, you can do SEO on your own, but it’s important to remember that it’s a long-term process that takes patience and diligence. Professional SEO services can help you stay on track and make sure your website is performing at its best.",
     ],
   },
   {
-    question: "6:30 award anouncement",
+    question: "Special guest speech",
     answer: [
       "When it comes to SEO, there are a lot of things to think about. You need to make sure that your website is coded in a way that makes it easy for search engines to crawl, that you have quality content, and that you’re doing everything you can to get links from other websites. If all of this sounds like too much for you, or if you just don’t have the time to do it yourself, you may want to consider hiring an SEO agency.",
     ],
   },
   {
-    question: "8:00 live concert by AGhori muzik team",
+    question: "Live Concert Muzik with Aghori",
     answer: [
       "Small businesses should be careful not to spend too much on SEO services each month. While it is important to invest in SEO, small businesses can quickly go over their budget if they’re not careful. It’s important to find a balance between the money you’re spending and the results you’re seeing.",
     ],
@@ -83,6 +84,9 @@ const plansData = [
 const Home = () => {
   return (
     <div className="text-[100px]">
+      <div className="">
+        <img src={banner} alt="" />
+      </div>
       <Timmer />
       <Video />
       <Schedule faqs={faqs} />
